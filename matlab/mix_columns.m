d3d95bb1e98e6e3760a746e3ed0adf93d71b0b0c
Farhan Bin Amin <farhanbinamin@gmail.com>
2022-06-30 17:45:49 +0600

    new: added `mix_columns` function

--- matlab/mix_columns.m
+++ matlab/mix_columns.m
@@ -1,15 +1,27 @@
 function [mix_columns_output] = mix_columns(shift_rows_output)
 % i/p > dec, 4x4
-% o/p > 
+% o/p > dec, 4x4
 
-shift_rows_output = dec2hex(shift_rows_output);
-% shift_rows_output = convertCharsToStrings(shift_rows_output)
-% shift_rows_output = reshape(shift_rows_output, [2, 16]);
+%  pdm = pre deined matrix
+pdm = [
+    2   3   1   1;
+    1   2   3   2;
+    1   1   2   3;
+    3   1   1   2];
 
-for i = 1:16
-    shift_rows_output(i) = convertCharsToStrings(shift_rows_output(i));
+% srw =  shift_rows_output converted from dec to hex
+sro = shift_rows_output;
+
+for col = 1:4
+    row = 1;
+    while(row <= 4)
+        a = pdm(row,1) * sro(row, col);
+        b = pdm(row,2) * sro(row, col);
+        c = pdm(row,3) * sro(row, col);
+        d = pdm(row,4) * sro(row, col);
+        mix_columns_output(row, col) = bitxor(bitxor(a, b), bitxor(c,d));
+        row = row + 1;
+    end
 end
-
-mix_columns_output = shift_rows_output;
 
 end
