ea01381058733934cdaa9ce144807d0eafbda041
Farhan Bin Amin <farhanbinamin@gmail.com>
2022-07-16 00:41:42 +0600

    fix: skip `mix_columns` in round 10

--- matlab/encryption/aes_encryption.m
+++ matlab/encryption/aes_encryption.m
@@ -10,18 +10,21 @@ initial_state = bitxor(plaintext_dec, initial_roundkey);
 % MAIN ROUND
 
 for i = 1: 10
-    % 1. Byte Substitution | sub_bytes
+    % 1. Byte Substitution
     sub_bytes_output = sub_bytes(initial_state);
 
-    % 2. Shift Rows | shift_rows
+    % 2. Shift Rows
     shift_rows_output = shift_rows(sub_bytes_output);
 
-    % 3 Mix Columns | mix_columns
-    if(i <= 9) % skip mix_columns for round 10 
+    % 3. Mix Columns
+    % skip mix_columns for round 10 
+    if(i <= 9)
         mix_columns_output = mix_columns(shift_rows_output);
+    else
+        mix_columns_output = shift_rows_output;
     end
 
-    % 4 Add Round Key | add_round_key
+    % 4. Add Round Key
     add_round_key_output = add_round_key(mix_columns_output, roundkeys{i});
 
     main_round_output = add_round_key_output;
--- matlab/encryption/mix_columns.m
+++ matlab/encryption/mix_columns.m
@@ -2,14 +2,14 @@ function [mix_columns_output] = mix_columns(shift_rows_output)
 % i/p > dec, 4x4
 % o/p > dec, 4x4
 
-%  pdm = pre deined matrix
+% pdm = pre deined matrix
 pdm = [
     2   3   1   1;
     1   2   3   2;
     1   1   2   3;
     3   1   1   2];
 
-% sro =  shift_rows_output converted from dec to hex
+% sro =  shift_rows_output
 sro = shift_rows_output;
 
 mix_columns_output = zeros(4);  % pre allocation
@@ -17,10 +17,10 @@ mix_columns_output = zeros(4);  % pre allocation
 for col = 1:4
     row = 1;
     while(row <= 4)
-        a = pdm(row,1) * sro(row, col);
-        b = pdm(row,2) * sro(row, col);
-        c = pdm(row,3) * sro(row, col);
-        d = pdm(row,4) * sro(row, col);
+        a = pdm(row, 1) * sro(row, col);
+        b = pdm(row, 2) * sro(row, col);
+        c = pdm(row, 3) * sro(row, col);
+        d = pdm(row, 4) * sro(row, col);
         mix_columns_output(row, col) = bitxor(bitxor(a, b), bitxor(c,d));
         row = row + 1;
     end
