96f370bc28e9b2af6b3f8f41f803fc1059e312ea
Farhan Bin Amin <farhanbinamin@gmail.com>
2022-08-11 01:05:04 +0600

    new: added verilog code for `ram_4x8`

--- verilog/ram_4x8.v
+++ verilog/ram_4x8.v
@@ -1,42 +1,34 @@
-//-----------------------------------------------------
+//--------------------------------------------
 // ram_4x8
-// 4 = 4 bit data
-// 8 = total no of addresses; 2^3 = 8, address = 3 bit
-//-----------------------------------------------------
+// data = 4-bit
+// address = 3-bit, total addresses = 2^n = 2^3 = 8
+//--------------------------------------------
+
 module ram_4x8 (
 clk     , // clock
 addr    , // address
 data_in , // data input
 data_out, // data output
-cs      , // chip select
-we        // write enable/ read enable
-); 
+rw        // read write
+);
 
-parameter DATA_WIDTH = 4;
-parameter ADDR_WIDTH = 3;
-parameter RAM_DEPTH = 8 ;
+input            clk     ;
+input      [2:0] addr    ; // 3-bit addresses
+input      [3:0] data_in ; // 4-bit input data
+output reg [3:0] data_out; // 4-bit output data
+input            rw      ; // read write
 
-input                   clk     ;
-input  [ADDR_WIDTH-1:0] addr    ;
-input  [DATA_WIDTH-1:0] data_in ;
-output [DATA_WIDTH-1:0] data_out;
-input                   cs      ;
-input                   we      ;
+reg [3:0] ram [7:0]; // ram_4x8; reg [each-data-bit] mem [total-addresses-bit]
 
-reg [DATA_WIDTH-1:0] ram [0:RAM_DEPTH-1]; // ram 4x8
-reg [ADDR_WIDTH-1:0] addr_reg; // address register
-
-//--------------Code Starts Here------------------ 
-// WRITE; we = 1, cs = 1
-// READ;  we = 0, cs = 1
+// WRITE; rw = 0
+// READ;  rw = 1
 always @ (posedge clk) begin
-   if ( we ) begin
+   if ( !rw ) begin
        ram[addr] <= data_in;
+       data_out = 4'b0;
    end else begin
-       addr_reg = addr;
+       data_out <= ram[addr];
    end
 end
-
-assign data_out = ram[addr_reg];
 
 endmodule
---
+++ verilog/ram_4x8_tb.v
@@ -0,0 +1,30 @@
+module ram_4x8_tb();
+
+reg       clk     ;
+reg  [2:0]addr    ;
+reg  [3:0]data_in ;
+wire [3:0]data_out;
+reg       rw      ;
+
+always
+#10 clk <= ~clk;
+
+initial
+begin
+// WRITE
+#0     clk = 0;
+#10    rw = 0;    addr = 3'b000;    data_in = 4'b0000;
+#10    rw = 0;    addr = 3'b001;    data_in = 4'b0001;
+#10    rw = 0;    addr = 3'b010;    data_in = 4'b0010;
+#10    rw = 0;    addr = 3'b011;    data_in = 4'b0011;
+
+// READ
+#10    rw = 1;    addr = 3'b000;
+#10    rw = 1;    addr = 3'b001;
+#10    rw = 1;    addr = 3'b010;
+#10    rw = 1;    addr = 3'b011;
+end
+
+ram_4x8 ram_4x8_ins(.clk(clk), .addr(addr), .data_in(data_in), .data_out(data_out), .rw(rw));
+
+endmodule
